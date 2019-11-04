3b26bb20dd996e875cf0a2c61d6febdf8cae840f
kolomu <kevin-olomu@web.de>
2019-11-04 17:55:40 +0100

    added board and squares

---
+++ notes.md
@@ -0,0 +1,2 @@
+# Notes
+Dumb component = easy 2 test & only interested in UI
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { runInThisContext } from 'vm';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: any[]; // 9 moves on gameboard
+  xIsNext: boolean; // determine current player
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  // create a computed property by using typescript getter
+  get player() {
+    return this.xIsNext ? 'X' : 'O'
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner(this.squares);
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+}
